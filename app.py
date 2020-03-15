b35a8b922f1da72ed50ef51b9a7360a0e2f989f3
djura <djura@smitsanalytics.com>
2020-03-15 12:22:16 +0100

    Made port configurable

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 # -*- coding: utf-8 -*-
+import sys
+
 import dash
 import dash_html_components as html
 import dash_table
 
 import data
+
+DEFAULT_PORT = 8050
 
 df = data.get_data()
 
@@ -20,5 +24,15 @@ app.layout = html.Div(children=[
 
 ])
 
+
+def main():
+    port = DEFAULT_PORT
+    if len(sys.argv) > 1:
+        port = sys.argv[1]
+        port = int(port)
+
+    app.run_server(debug=True, port=port)
+
+
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    main()
