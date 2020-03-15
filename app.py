d75e905726f9bf73e3d5d319e4a28b67fce0e39c
djura <djura@smitsanalytics.com>
2020-03-15 15:28:35 +0100

    Created interactive table Added some styling

--- app.py
+++ app.py
@@ -2,35 +2,45 @@
 import os
 
 import dash
+import dash_core_components as dcc
 import dash_html_components as html
 import dash_table
 
 import data
 
-DEFAULT_PORT = 8050
+_DEFAULT_PORT = 8050
+_COLUMNS = ['Date', 'CountryName', 'Confirmed', 'Deaths']
 
 df = data.get_data()
 
 app = dash.Dash(__name__)
 server = app.server
 
-app.layout = html.Div(children=[
-    html.H1(children='Covid-19 data visualization'),
+app.layout = \
+    html.Div(className='app-container',
+             children=[
+                 html.H1(children='Covid-19 data visualization'),
+                 dcc.Markdown(
+                     'Visualization of historical data on covid-19 spread and deaths using the Open [COVID-19 Dataset](https://github.com/open-covid-19/data)'),
+                 dash_table.DataTable(
+                     id='table',
+                     columns=[{"name": i, "id": i} for i in _COLUMNS],
+                     data=df.to_dict('records'),
+                     style_table={
+                         'maxHeight': '300px',
+                         'overflowY': 'scroll'
+                     },
+                     filter_action="native",
+                     sort_action="native",
+                     sort_mode="multi",
+                 )
 
-    dash_table.DataTable(
-        id='table',
-        columns=[{"name": i, "id": i} for i in df.columns],
-        data=df.to_dict('records')
-    )
-
-])
+             ])
 
 
 def main():
-    port = DEFAULT_PORT
-    if os.environ['PORT']:
-        port = os.environ['PORT']
-        port = int(port)
+    port = os.environ.get('PORT', 8080)
+    port = int(port)
 
     app.run_server(debug=True, port=port)
 
