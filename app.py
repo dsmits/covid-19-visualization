d0ff0e5e4b727ef083503363d89006723fc1d838
djura <djura@smitsanalytics.com>
2020-03-15 16:15:32 +0100

    Created graph that is updated by changing table

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import dash_table
+import pandas as pd
+from dash.dependencies import Output, Input
 
 import data
 
@@ -23,7 +25,7 @@ app.layout = \
                  dcc.Markdown(
                      'Visualization of historical data on covid-19 spread and deaths using the Open [COVID-19 Dataset](https://github.com/open-covid-19/data)'),
                  dash_table.DataTable(
-                     id='table',
+                     id='coviz-table',
                      columns=[{"name": i, "id": i} for i in _COLUMNS],
                      data=df.to_dict('records'),
                      style_table={
@@ -33,9 +35,37 @@ app.layout = \
                      filter_action="native",
                      sort_action="native",
                      sort_mode="multi",
-                 )
+                 ),
 
+                 dcc.Graph(
+                     id='coviz-graph'
+                 )
              ])
+
+
+@app.callback(
+    Output('coviz-graph', 'figure'),
+    [Input('coviz-table', 'derived_virtual_data')])
+def display_output(rows):
+    filtered_df = pd.DataFrame(rows, columns=_COLUMNS)
+
+    agg = filtered_df.groupby('Date').agg({'Confirmed': 'sum', 'Deaths': 'sum'})
+
+    return {
+        'data':
+            [
+                {'x': agg.index,
+                 'y': agg['Confirmed'],
+                 'type': 'line',
+                 'name': 'Confirmed cases'
+                 },
+                {'x': agg.index,
+                 'y': agg['Deaths'],
+                 'type': 'line',
+                 'name': 'Deaths'
+                 }
+            ]
+    }
 
 
 def main():
